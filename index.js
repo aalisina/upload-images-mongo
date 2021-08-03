66eaff38d889f4c99078fce3fe08b2af62f9458e
sinazay123 <sinazay123@gmail.com>
2021-08-02 21:08:24 -0500

    agregar rutas de subir imagines

--- index.js
+++ index.js
@@ -56,7 +56,24 @@ const upload = multer({storage})
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+  gfs.files.find().toArray( (err, files)=> {
+    // Check if files exist
+    if(!files || files.length === 0) {
+      res.render('index', {files: false})
+    } else {
+      files.map( (file) => {
+        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+          file.isImage = true
+        } else {
+          file.isImage = false;
+        }
+      })
+      res.render('index', {files: files})
+    }
+
+    // FIles do exist
+    return res.json(files)
+  } )
 });
 
 // @route POST upload
@@ -66,7 +83,62 @@ app.post('/upload', upload.single('file'), (req, res)=> {
     res.redirect('/')
 })
 
-//
+//@ route GET /files
+//@desc Display all files in json
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray( (err, files)=> {
+    // Check if files exist
+    if(!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files found.'
+      });
+    }
+
+    // FIles do exist
+    return res.json(files)
+  } )
+})
+
+//@ route GET /files/:filename
+//@desc Display one file in json
+app.get('/files/:filename', (req, res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file)=> {
+      // Check if file exists
+      if(!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file found.'
+        });
+      }
+    // File found
+    return res.json(file);
+  })
+})
+
+//@ route GET /images/:filename
+//@desc Display an image
+app.get('/images/:filename', (req, res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file)=> {
+      // Check if file exists
+      if(!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file found.'
+        });
+      }
+    // Check if it is an image
+    if(file.contentType === 'image/jpeg' || file.contentType === 'img/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+
+    } else {
+      res.status(400).json({
+        err: 'Not an image.'
+      })
+    }
+  })
+})
+
+
 
 const PORT = 3001;
 
